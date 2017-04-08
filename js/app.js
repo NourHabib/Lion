fc84e6b1d371ee79d6fccd8b0c2e24c5cc2e3b38
Nour <nourhabib8@gmail.com>
2017-04-08 18:29:51 +0300

    Adding Eyecolor duplicate same structure for hair color

--- js/app.js
+++ js/app.js
@@ -44,5 +44,23 @@ mainApp.controller('lionCtrl', function($scope,$http) {
 		{shape:"short",value:2},
 		{shape:"long",value:3}
 	];
+	$scope.eyecolors=[
+		{color:'black',value: 1},
+		{color:"azure",value:2},
+		{color:"blue",value:3},
+		{color:"brown",value:4},
+		{color:"cornsilk",value:5},
+		{color:"dark-orange",value:6},
+		{color:"dark-violet",value:7},
+		{color:"green",value:8},
+		{color:"grey",value:9},
+		{color:"olive",value:10},
+		{color:"orange",value:11},
+		{color:"red",value:12},
+		{color:"violet",value:13},
+		{color:"white",value:14},
+		{color:"yellow",value:15},
+		{color:"wood",value:16}
+	];
 
  });
