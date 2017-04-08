dfc94fe814ead99fe7c881d9e0c02b27f29d54cd
Nour <nourhabib8@gmail.com>
2017-04-08 04:03:28 +0300

    Adding angular slider module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-var mainApp = angular.module("lionApp", ['']);
+var mainApp = angular.module("lionApp", ['rzModule']);
 
- mainApp.controller('lionCtrl', function($scope,$http) {
+mainApp.controller('lionCtrl', function($scope,$http) {
+
+
+ 	$scope.ageslider = {
+    	value: 20,
+    	options: {
+	      floor: 0,
+	      ceil: 100,
+	      step: 1,
+	      minLimit: 0,
+	      maxLimit: 100
+	    }
+	};
 
  });
